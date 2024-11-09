ee36f1e97275606416c5ff0f31d3eca49c1a6495
AimanManaf <aiman.muhammad88@gmail.com>
2024-11-09 23:43:49 +0800

    Add Main.java
    random things

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Please enter your name:");
+        Scanner sc = new Scanner(System.in);
+        String myName = sc.nextLine();
+        System.out.println("Your name is " + myName);
+    }
+}
+
